2980be76a5e9b1a44f58ab5dababe37766fea861
damianjnc <damianjnc@gmail.com>
2019-01-28 09:25:53 +0000

    portfolio section

--- js/script.js
+++ js/script.js
@@ -34,6 +34,8 @@ $(document).ready(function () {
 
 
 var skillTopOffset = $(".skillsSection").offset().top;
+var statsTopOffset = $(".statsSection").offset().top;
+var countUpFinished = false;
 $(window).scroll(function(){
    if(window.pageYOffset > skillTopOffset - $(window).height() + 200){
       $('.chart').easyPieChart({
@@ -51,9 +53,42 @@ $(window).scroll(function(){
 
 
    }
-
+   if(!countUpFinished &&  window.pageYOffset > statsTopOffset - $(window).height() + 200){
+      $(".counter").each(function(){
+         var element = $(this);
+         var endVal =parseInt(element.text());
+         element.countup(endVal);
+      });
+      countUpFinished = true;
+   }
 
 });  
+   $("[data-fancybox]").fancybox();
 
+   $(".items").isotope({
+      filter:'*',
+      animationOptions: {
+         duration: 1500,
+         easing: 'linear',
+         queue: false
+      }
+   });
+
+   $("#filters a").click(function(){
+      $("#filters .current").removeClass("current");
+      $(this).addClass("current");
+
+      var selector = $(this).attr("data-filter");
+
+      $(".items").isotope({
+         filter:selector,
+         animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+         }
+      });
+      return false;
+   });
 
 });
